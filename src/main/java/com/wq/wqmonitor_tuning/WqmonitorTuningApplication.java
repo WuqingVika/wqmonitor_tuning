b4e6a5a7fa76131c4a250801ce2c58a651071a7d
wuqingvika <526288022@qq.com>
2018-12-27 17:38:22 +0800

    项目初始化

---
+++ src/main/java/com/wq/wqmonitor_tuning/WqmonitorTuningApplication.java
@@ -0,0 +1,14 @@
+package com.wq.wqmonitor_tuning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WqmonitorTuningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WqmonitorTuningApplication.class, args);
+	}
+
+}
+
